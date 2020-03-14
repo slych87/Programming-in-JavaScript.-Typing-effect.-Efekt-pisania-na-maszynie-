9acdd8627edc2ff68bdebfa2856cb13b4f246164
Mateusz Słyszewski <mateuszslyszewski@gmail.com>
2020-03-14 17:54:23 +0100

    cursor

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const spnText = document.querySelector('.text');
-
+const spnCursor = document.querySelector('.cursor');
 const txt = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Corporis asperiores in sed praesentium vitae aliquam iusto iste, quis vel. Fuga quisquam porro minima nostrum ducimus pariatur quas accusamus possimus velit. KONIEC';
 
 //Parameters
@@ -12,5 +12,10 @@ const addLetter = () => {
     indexText++;
     if (indexText == txt.length) clearInterval(indexTyping);
 }
+const cursorAnimation = () => {
+    spnCursor.classList.toggle('active');
+
+}
 
 const indexTyping = setInterval(addLetter, time);
+setInterval(cursorAnimation, 400);
